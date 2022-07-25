efa665562ef4d6c5c365ea5486a6a7c4c41bbec7
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-25 16:36:32 +0530

    ADDED: Edit Contacts

--- src/Com/BridgeLabz/AddressBookCollection/AddressBook.java
+++ src/Com/BridgeLabz/AddressBookCollection/AddressBook.java
@@ -10,13 +10,13 @@ public class AddressBook {
      * Creating a List of ContactDetails of type String using ArrayList here Adding
      * new elements in the List
      */
-    static List<ContactDetails> contactDetailsList = new ArrayList();
+    static List<ContactDetails> contactDetailsList = new ArrayList<ContactDetails>();
+    static Scanner scanner = new Scanner(System.in);
 
     /**
      * created method addNewContact() to create a new contacts to the AddressBook
      */
-    public static void addNewContact() {
-        Scanner scanner = new Scanner(System.in);
+    public void addContact() {
         System.out.println("Enter First Name : ");
         String firstName = scanner.next();
         System.out.println("Enter Last Name : ");
@@ -33,7 +33,6 @@ public class AddressBook {
         long mobileNumber = scanner.nextLong();
         System.out.println("Enter your EmailId  : ");
         String emailId = scanner.next();
-        scanner.close();
 
         person = new ContactDetails(firstName, lastName, address, city, state, zipCode, mobileNumber, emailId);
         contactDetailsList.add(person);
@@ -43,6 +42,22 @@ public class AddressBook {
 
     /**
      * created method printContact() to display the data
+     */
+    public void editContact() {
+        System.out.println("Enter the first name of person to edit Contact : ");
+        String firstName = scanner.next();
+        if (firstName.equalsIgnoreCase(person.getFirstName())) {
+            addContact();
+        } else {
+            System.out.println("The Entered First Name Is Not Match");
+            editContact();
+        }
+    }
+
+    /**
+     * Main method for manipulation AddressBookCollection
+     *
+     * @param args - Default Java param (Not used)
      */
     public static void printContact() {
         for (int i = 0; i < contactDetailsList.size(); i++) {
@@ -54,18 +69,6 @@ public class AddressBook {
                     + "MobileNumber  : " + person.getMobileNumber() + "\n" + "EmailId       : " + person.getEmailId()
                     + "\n");
         }
-    }
-
-    /**
-     * Main method for manipulation AddressBookCollection
-     *
-     * @param args - Default Java param (Not used)
-     */
-    public static void main(String[] args) {
-        /**
-         * calling method addNewContact()
-         */
-        addNewContact();
     }
 
 }
