cb35bee99c31a3a08d874468aa0d3698cbcfdf3d
Akhil-Choudhari <akhil.choudhari.ac@gmail.com>
2022-07-25 16:18:55 +0530

    START: Welcome to the Address book program using collection

---
+++ src/Com/BridgeLabz/AddressBookCollection/AddressBook.java
@@ -0,0 +1,7 @@
+package Com.BridgeLabz.AddressBookCollection;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the address book program using collection");
+    }
+}
